8e1c2a0ac1e5d7f66fe1d10bfb90eb9b28566485
jburhan <jimburhan@gmail.com>
2018-07-03 15:47:46 -0700

    client-side-js completed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Jimmetrics
 
 This is a Rails app used to provide analytics service to track events on websites
+
+client-side JS snippet:
+var jimmetrics = {};
+  jimmetrics.report = function(eventName){
+    var event = {event: { name: eventName }};
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(event));
+  };
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
-//= require bootstrap
+var jimmetrics = {};
+  jimmetrics.report = function(eventName){
+    // creates a JSON event object and send it to the Jimmetrics
+    var event = {event: { name: eventName }};
+    // initializes a new XMLHttpRequest() (AJAX request)
+    var request = new XMLHttpRequest();
+    // sets the HTTP method to POST, the url of our events endpoint, and allows the request to run asynchronously.
+    request.open("POST", "http://localhost:3000/api/events", true);
+    // sets the request Content-Type header to 'application/json'. Without that, our API::EventsController would not know to process the request as JSON.
+    request.setRequestHeader('Content-Type', 'application/json');
+    // sends the request.
+    request.send(JSON.stringify(event));
+  };
